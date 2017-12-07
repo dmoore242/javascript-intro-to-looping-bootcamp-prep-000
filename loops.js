1bdd0f790439747f420e43fc51a4145d3b2b49f5
Devin Moore <devin.moore@volken.org>
2017-12-07 21:35:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,15 @@
 function forLoop(array) {
   for (let i=0; i < 25; i++) {
     if(i===1) {
-      console.log(array.push(`"I am ${i} strange loop"`));
+      array.push(`"I am ${i} strange loop"`)
     } else {
-      console.log(array.push(`"I am ${i} strange loops."`));
+      console.log(array.push(`"I am ${i} strange loops."`)
     }
   }
 }
 
+function whileLoop(number) {
+  while (countdown > 0) {
+    console.log()
+  }
+}
