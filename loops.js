127553d812571cce2244a1cccef6c06bbdda009a
Devin Moore <devin.moore@volken.org>
2017-12-07 21:20:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i=0; i < 25; i++) {
-    if(i) {
-      console.log(`"I am" + ${i} + "strange loop."`);
+    if(i===1) {
+      return array.push(`"I am" + ${i} + "strange loop."`);
     } else {
-      console.log(`'I am' + ${i} + "strange loops."`)
+      return array.push(`'I am' + ${i} + "strange loops."`)
     }
   }
 }
